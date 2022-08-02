2b245c2f358d5574b4f1ce77d43349f2f9f535a3
Tokarev-Alexey <leha_motaro@mail.ru>
2022-08-02 18:43:56 +0700

    Created a publication page and a news feed 2.

---
+++ blog/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.forms import TextInput,Textarea
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+        widgets = {
+            'title': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 600px;',
+                'placeholder': 'Title',
+            }),
+            'text': Textarea(attrs={
+                'class': "form-control",
+                'style': 'max-width: 600px;',
+                'placeholder': 'Text'
+            })
+        }
