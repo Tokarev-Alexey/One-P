76934fff2ce5b58320fdc29e65c33b8a115f5989
Tokarev-Alexey <leha_motaro@icloud.com>
2024-09-15 14:26:23 +0700

    страховочка

--- blogRestAPI/urls.py
+++ blogRestAPI/urls.py
@@ -6,11 +6,11 @@ from .views import PostViewSet, CommentViewSet, PostViewSetDetail
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
-router.register(r'post', PostViewSetDetail)
+router.register(r'post', PostViewSetDetail, basename='something-unique')
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/info/', views.load_statistic, name='loadstatistic'),
-    ]
+]
